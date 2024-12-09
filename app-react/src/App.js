76659b5480759d8d661fd4f8a38aad2047d1d913
Théo Gaillard <theo.gaillard@sage.com>
2024-12-09 15:15:08 +0100

    'carbon'

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import "carbon-react/lib/style/fonts.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <ul class="navbar">
+    <li>
+      <a href="http://localhost:3000/">Accueil</a>
+      <a href="http://localhost:3000/">Patchs</a>
+      <a href="http://localhost:3000/">Tests-unitaires</a>
+    </li>
+      </ul>
       </header>
        <section>
         <h1>Pour plus d'information</h1>
+        <a href="https://developer.mozilla.org/fr/" target="_top">Visitez MDN Web Docs</a>
+        <h2>a-b</h2>
        </section>
     </div>
   );
--- app-react/src/index.js
+++ app-react/src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import CarbonProvider from "carbon-react/lib/components/carbon-provider";
+import GlobalStyle from "carbon-react/lib/style/global-style";
+import sageTheme from "carbon-react/lib/style/themes/sage";
+import "carbon-react/lib/style/fonts.css";
+import Typography from "carbon-react/lib/components/typography";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const CarbonApp = (props) => {
+  return (
+    <CarbonProvider theme={sageTheme}>
+      <GlobalStyle />
+      <Typography></Typography>
+      <App />
+    </CarbonProvider>
+  );
+};
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CarbonApp />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
